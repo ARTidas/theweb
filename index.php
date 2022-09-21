a899cb1816db438266dc2cd4b0b8a9556b01b72d
Veres Zoltán <artidas@artidas.hu>
2022-09-21 16:06:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+  header("Cache-Control: no-cache, no-store, must-revalidate");
+  header("Pragma: no-cache");
+  header("Expires: 0");
+
+  error_reporting(E_ALL & ~E_NOTICE);
+  ini_set('display_errors', 1);
+
+  require(dirname(__FILE__) . '/models/helpers/log_helper.php');
+
+  LogHelper::add('--------------------------------------------------------------------------------');
+  LogHelper::add(date('Y-m-d H:i:s', time()));
+  LogHelper::add('Starting up engines...');
+
+  
   echo('Hello world...');
 
 
